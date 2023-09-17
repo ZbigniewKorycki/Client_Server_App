f219bb77ea7234bc588d7948fa97a63905771e8d
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-09-17 12:32:58 +0200

    add commands description for command "help"

---
+++ commands.py
@@ -0,0 +1,6 @@
+commands_description = {
+    "uptime": "returns server uptime",
+    "info": "returns the version number of the server and the date it was created",
+    "help": "returns the list of available commands with a short description",
+    "stop": "stops the server and the client simultaneously"
+}
