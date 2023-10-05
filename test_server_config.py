c13f11fc919a3698efa2b5ace3a474fd8b0a4c49
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-05 11:00:30 +0200

    test, check generated password - correct length, correct type, minimum 8 random symbols

--- test_server_config.py
+++ test_server_config.py
@@ -6,9 +6,17 @@ class TestServer(unittest.TestCase):
     def setUp(self):
         self.server = Server('192.168.0.163', 61033, 1024)
 
-    def test_should_server_return_generated_password(self):
+    def test_should_server_return_generated_password_with_correct_length(self):
+        result = len(self.server.password_generator())
+        self.assertEqual(result, 12)
+
+    def test_should_server_return_generated_password_of_type_str(self):
         result = self.server.password_generator()
-        self.assertEqual(len(result), 12)
+        self.assertIsInstance(result, str)
+
+    def test_should_server_return_generated_password_with_at_least_8_random_symbols(self):
+        result = len(set(self.server.password_generator()))
+        self.assertGreaterEqual(result, 8)
 
 
 if __name__ == '__main__':
