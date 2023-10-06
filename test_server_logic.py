f48b59ba4fbd8fec7bd10747c4a89de4f9f1f671
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-06 10:19:30 +0200

    add tests to recognize admin privileges, send message to not existed recipient, send message over 255 symbols

--- test_server_logic.py
+++ test_server_logic.py
@@ -1,6 +1,7 @@
 import unittest
 from server_logic import Server
 
+
 class TestServerLogic(unittest.TestCase):
 
     def setUp(self):
@@ -27,9 +28,9 @@ class TestServerLogic(unittest.TestCase):
         self.assertEqual(result, 'The user have to start with a letter.')
 
     def test_should_server_dont_add_user_with_existed_username(self):
-        user1st = self.server.add_user(username="Mario")
-        result = self.server.add_user(username="Mario")
-        self.assertEqual(result, 'The user with this name exists, choose another username.')
+        user_first = self.server.add_user(username="Mario")
+        user_duplicate = self.server.add_user(username="Mario")
+        self.assertEqual(user_duplicate, 'The user with this name exists, choose another username.')
 
     def test_should_login_in_with_correct_data(self):
         new_user = self.server.add_user(username="Marek")
@@ -44,12 +45,43 @@ class TestServerLogic(unittest.TestCase):
         result = self.server.login_into_system(new_user_username, new_user_password)
         self.assertFalse(result)
 
+    def test_should_server_recognize_admin_privilege(self):
+        new_admin = self.server.add_user(username="test_admin", privilege="admin")
+        result = self.server.check_if_admin(self.server.users[0])
+        self.assertTrue(result)
 
+    def test_should_server_recognize_if_no_admin_privilege_only_basic_user(self):
+        new_admin = self.server.add_user(username="test_admin", privilege="user")
+        result = self.server.check_if_admin(self.server.users[0])
+        self.assertFalse(result)
 
+    def test_should_server_recognize_if_incorrect_privilege_given(self):
+        new_admin = self.server.add_user(username="test_admin", privilege="random_privilege")
+        result = self.server.check_if_admin(self.server.users[0])
+        self.assertFalse(result)
 
+    def test_should_send_message_to_not_existed_recipient(self):
+        user_sender = self.server.add_user(username="user1")
+        result = self.server.send_message(sender="user1", recipient="user2", message="test_message")
+        self.assertEqual(result, "The recipient does not exist.")
+
+    def test_should_send_message_with_over_255_symbols(self):
+        user_sender = self.server.add_user(username="user1")
+        user_recipient = self.server.add_user(username="user2")
+        result = self.server.send_message(sender="user1", recipient="user2",
+                                          message="Lorem Ipsum is simply dummy text of the printing and"
+                                                  " typesetting industry. Lorem Ipsum has been the industry's"
+                                                  " standard dummy text ever since the 1500s, when an unknown"
+                                                  " printer took a galley of type and scrambled it to make"
+                                                  " a type specimen book. It has survived not only five centuries,"
+                                                  " but also the leap into electronic typesetting,"
+                                                  " remaining essentially unchanged."
+                                                  " It was popularised in the 1960s with the release of"
+                                                  " Letraset sheets containing Lorem Ipsum passages, and more"
+                                                  " recently with desktop publishing software like"
+                                                  " Aldus PageMaker including versions of Lorem Ipsum.")
+        self.assertEqual(result, "Message is too long (max. 255 characters).")
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
