e58db16ace9804a715dfcb6cd5a46b4959cb53e6
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-11 12:41:52 +0200

    add class to sql connection, add function connect, execute query, close connection

--- db_connection.py
+++ db_connection.py
@@ -1,27 +1,40 @@
 import psycopg2
 from db_config import config
 
+config_data = config()
 
-def connect():
-    connection = None
-    try:
-        params = config()
-        connection = psycopg2.connect(**params)
-        cursor = connection.cursor()
-        print('PostgreSQL database version:')
-        cursor.execute('SELECT version()')
-        db_version = cursor.fetchone()
-        print(db_version)
+
+class PostgresSQLConnection:
+
+    def __init__(self):
+        self.dbname = config_data['database']
+        self.user = config_data['user']
+        self.password = config_data['password']
+        self.host = config_data['host']
+        self.port = config_data['port']
+        self.connection = None
+
+    def connect_with_db(self):
+        self.connection = psycopg2.connect(
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port)
+
+    def execute_query(self, query, params):
+        if not self.connection:
+            self.connect_with_db()
+        self.connection.autocommit = False
+        cursor = self.connection.cursor()
+        cursor.execute(query, params)
+        result = cursor.fetchall()
         cursor.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if connection is not None:
-            connection.close()
-            print("PostgreSQL connection is closed")
+        return result
 
-
-if __name__ == '__main__':
-    connect()
+    def close_connection_with_db(self):
+        if self.connection:
+            self.connection.close()
+            self.connection = None
 
 
