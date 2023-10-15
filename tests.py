3e835f25b91b2a6efa083b3dd7d977c41f2f6cf9
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-15 17:56:34 +0200

    move all tests to one file

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from server_logic import Server
 import datetime
 import commands
 from db_connection import PostgresSQLConnection
+import psycopg2
 
 
 class TestServerLogic(unittest.TestCase):
@@ -226,6 +227,34 @@ class TestCommandsDescription(unittest.TestCase):
         self.assertIn("delete-user", result)
         self.assertIn("change-privileges", result)
 
+class TestDatabaseConnection(unittest.TestCase):
+
+    def setUp(self):
+        self.db = PostgresSQLConnection(dbname="test_db")
+
+    def tearDown(self):
+        result = self.db.database_transaction(
+            query="""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'""")
+        table_names = [table_name[0] for table_name in result]
+        for table in table_names:
+            self.db.database_transaction(f"""DROP TABLE IF EXISTS {table} CASCADE;""")
+
+    def test_open_close_connection_with_db(self):
+        self.assertEqual(self.db.connection, None)
+        self.db.connect_with_db()
+        self.assertNotEqual(self.db.connection, None)
+        self.assertIsInstance(self.db.connection, psycopg2.extensions.connection)
+        self.db.close_connection_with_db()
+        self.assertEqual(self.db.connection, None)
+        self.assertNotIsInstance(self.db.connection, psycopg2.extensions.connection)
+
+    def test_database_transaction(self):
+        result_incorrect_syntax = self.db.database_transaction("""INCORRECT QUERY test_table VALUES ("value");""")
+        result_table_dont_exists = self.db.database_transaction(
+                    query="""INSERT INTO test_table VALUES (%s, %s);""",
+                    params=("1.9", 199))
+        self.assertFalse(result_incorrect_syntax)
+        self.assertFalse(result_table_dont_exists)
 
 if __name__ == '__main__':
     unittest.main()
